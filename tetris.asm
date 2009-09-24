68f87bd9ce9cc2a5f9a3d6249d8fe7eb3362b330
Christoph Koehler <christoph@zerodeviation.net>
2009-09-24 13:49:02 -0500

    create tetris asm stub and move all the thoughts into a txt file

--- tetris.asm
+++ tetris.asm
@@ -1,45 +1,17 @@
-===========
-= Display =
-===========
-16 bit width, 32 bit height
-value 1 for block, value 0 for empty
-
- ==============
- = Event Loop =
- ==============
+* =============
+* = Variables =
+* =============
 
 
+* ========
+* = Main =
+* ========
 
- =========
- = Moves =
- =========
- Use interrupt
- 
-1. check collision
-1.T move piece
-1.F ignore input
 
-2. Check full row
-2.T remove line, shift everything above down
-2.F continue
+* ========
+* = Subs =
+* ========
 
-3. Reset timer
-
- =========
- = Timer =
- =========
-Every n seconds, shift current piece down by 1.
-
-1. Check collision downward
-1.T done with this piece, start over with new piece
-1.F move down by 1
-
- ==========
- = Pieces =
- ==========
- 1. Block
- 2. L
- 3. T
- 4. Z
- 5. Inverse Z
- 6. Line
+* ===========
+* = Vectors =
+* ===========
