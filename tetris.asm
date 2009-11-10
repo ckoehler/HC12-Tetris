b94d0c9c8e18264c4427963abefb17f8ebb3991b
Christoph Koehler <christoph@zerodeviation.net>
2009-11-10 14:10:57 -0600

    change comment

--- tetris.asm
+++ tetris.asm
@@ -36,7 +36,7 @@ buttons2	rmb	1
 buttons1l	rmb	1
 buttons2l	rmb	1
 
-* points somewhere in the stage to the bottom
+* points somewhere in the stage to the top
 * of the current block
 block_ptr	rmb	1
 
