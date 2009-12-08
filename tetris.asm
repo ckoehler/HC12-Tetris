ac9f70ce52b3fca500502b934b53037759ae8607
David Ibach <david.ibach@ou.edu>
2009-12-08 15:48:02 -0600

    Updated Score and stage to draw

--- tetris.asm
+++ tetris.asm
@@ -86,7 +86,7 @@ sav_block_ptr	rmb	2
 sav_shft_offset	rmb	1
 sav_rot_offset	rmb	1
 
-Time            rmb     1
+Time            	rmb     	1
 
 	org	$1000
 * ========
@@ -155,6 +155,8 @@ InitTimer:
 	rts
 	
 InitStage:
+	jsr	DrawStage
+	jsr	ScoreKeeper
 	jsr	determine_block
 	jsr 	serve_block
 	rts
